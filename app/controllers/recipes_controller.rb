6395c599ecc3e895f41e81b6e0d78573288cdb62
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 13:40:56 -0500

    Finish simple crud

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def new
@@ -10,9 +11,17 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def create
+    Recipe.create(recipe_params)
+
+    redirect_to recipes_path
+  end
+
   def update
     recipe = Recipe.find(params[:id])
     recipe.update(recipe_params)
+
+    redirect_to recipes_path
   end
 
   def recipe_params
