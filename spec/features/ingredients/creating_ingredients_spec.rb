b108678c618c9aba2a3954673dbaa6992beb30e3
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 12:38:56 -0500

    Add feature specs

---
+++ spec/features/ingredients/creating_ingredients_spec.rb
@@ -0,0 +1,19 @@
+describe "creating ingredients" do
+  context "on the new ingredient page" do
+    before do
+      visit new_ingredient_path
+    end
+
+    it "should have a form to create the ingredients" do
+      expect(page).to have_css("form#new_ingredient")
+    end
+
+    it "should create an ingredient when the form is submitted" do
+      fill_in 'ingredient_name', with: 'Parsley'
+      click('Create Ingredient')
+
+      Ingredient.first.name.should == "Parsley"
+      expect(page).to have_content("Parsley")
+    end
+  end
+end
