4ec1df4d5bfb7713d0c310b4a0d1524a232a6101
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 13:00:21 -0500

    stuff

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,12 +1,16 @@
 class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
   def new
     @ingredient = Ingredient.new
   end
 
   def create
-    ingredient = Ingredient.create(ingredient_params)
+    Ingredient.create(ingredient_params)
 
-    redirect_to edit_ingredient_path(ingredient)
+    redirect_to ingredients_path
   end
 
   private 
