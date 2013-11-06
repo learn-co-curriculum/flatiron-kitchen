126cf990e774745bbd0f4d30c9d739bb3541b78e
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 12:45:42 -0500

    Start ingredient solution

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,42 +1,15 @@
 class IngredientsController < ApplicationController
-  before_action :set_ingredient, only: [:edit, :update]
-
-  def index
-    @ingredients = Ingredient.all
-  end
-
   def new
     @ingredient = Ingredient.new
   end
 
   def create
-    ingredient = Ingredient.new(ingredient_params)
+    ingredient = Ingredient.create(ingredient_params)
 
-    if ingredient.save
-      redirect_to ingredients_path
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    @ingredient.update(ingredient_params)
-
-    if @ingredient.save
-      redirect_to ingredients_path
-    else
-      render :edit
-    end
+    redirect_to edit_ingredient_path(ingredient)
   end
 
   private 
-
-  def set_ingredient
-    @ingredient = Ingredient.find(params[:id])
-  end
 
   def ingredient_params
     params.require(:ingredient).permit(:name)
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131106182601) do
+ActiveRecord::Schema.define(version: 20131106174206) do
 
   create_table "ingredients", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "recipes", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
