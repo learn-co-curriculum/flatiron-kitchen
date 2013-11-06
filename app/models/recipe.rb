52ac0271f1cc289a7cf039b80d4cc3fe3a471e8f
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 14:13:20 -0500

    Form + join for recipe ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ActiveRecord::Base
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+  def update_ingredients_by_id(ingredient_ids)
+    ingredients = Ingredient.find(:all, :conditions => ["id in (?)", ingredient_ids])
+    self.ingredients = ingredients
+  end
 end
