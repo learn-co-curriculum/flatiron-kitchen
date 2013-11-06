f7beb6b8f0d43739701c2f4ed9b592c2bda55820
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 17:48:17 -0500

    Add helpful comments

--- spec/features/ingredients/editing_ingredients_spec.rb
+++ spec/features/ingredients/editing_ingredients_spec.rb
@@ -7,10 +7,15 @@ describe "editing ingredients" do
       visit edit_ingredient_path(@ingredient)
     end
 
+    # Is there a form with the given id? (check out how it's
+    # interpolated below)
     it "should have a form to edit the ingredient" do
       expect(page).to have_css("form#edit_ingredient_#{@ingredient.id}")
     end
 
+    # Does the recipe name get updated when the form is submitted?
+    # Does the user get redirected to a page that displays the recipe
+    # name?
     it "should update an ingredient when the form is submitted" do
       fill_in 'ingredient_name', with: 'Rochester Pollywog Eggs'
       click_button('Update Ingredient')
--- spec/features/recipes/creating_recipes_spec.rb
+++ spec/features/recipes/creating_recipes_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 describe "creating recipes" do
   context "on the new recipe page" do
+
+    # Is there a form with the given HTML ID?
     it "should have a form to create the recipes" do
       visit new_recipe_path
 
       expect(page).to have_css("form#new_recipe")
     end
 
+    # Does the recipe get created?
+    # Is the user redirected to a page that displays the recipe name?
     it "should create a recipe when the form is submitted" do
       visit new_recipe_path
 
@@ -18,6 +22,10 @@ describe "creating recipes" do
       expect(page).to have_content("Candy Corn Dumplings")
     end
 
+    # Does the recipe get successfully created with 1 ingredient?
+    # HINT: You need to use checkboxes. Each checkbox should have a CORRECTLY
+    #       implemented HTML label
+    #       (i.e. clicking on the <label> checks/unchecks the box).
     it "should create a recipe with one ingredient" do
       Ingredient.create(name: 'Spam')
 
@@ -31,6 +39,11 @@ describe "creating recipes" do
       Recipe.first.ingredients.where(name: 'Spam').count.should == 1
     end
 
+
+    # Does the recipe get successfully created with many ingredients?
+    # HINT: You need to use checkboxes. Each checkbox should have a CORRECTLY
+    #       implemented HTML label
+    #       (i.e. clicking on the <label> checks/unchecks the box).
     it "should create a recipe with many ingredients" do
       Ingredient.create(name: 'Paprika')
       Ingredient.create(name: 'Clove')
@@ -51,6 +64,11 @@ describe "creating recipes" do
       Recipe.first.ingredients.count.should == 4
     end
 
+
+    # Does the recipe get successfully created with 0 ingredients?
+    # HINT: You need to use checkboxes. Each checkbox should have a CORRECTLY
+    #       implemented HTML label
+    #       (i.e. clicking on the <label> checks/unchecks the box).
     it "should create a recipe with 0 ingredients" do
       visit new_recipe_path
 
