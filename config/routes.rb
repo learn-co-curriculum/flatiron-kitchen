973ed3a79c2d3e2e3e511eff3c549d1b7659fb05
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 13:00:21 -0500

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ FlatironKitchen::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :ingredients
-  resources :recipes
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
