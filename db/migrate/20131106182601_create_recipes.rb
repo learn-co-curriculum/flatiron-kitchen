f08df07c1a8aafb73fb3f3157fe3e2d968819fba
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 13:33:55 -0500

    stuff

--- db/migrate/20131106182601_create_recipes.rb
+++ db/migrate/20131106182601_create_recipes.rb
@@ -1,9 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      t.string :name
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
