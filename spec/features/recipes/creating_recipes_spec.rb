474397e2ee7a6be38b536e144a9ddeecdae45a2d
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 15:18:45 -0500

    Add tests for ingredient addition on new recipe page

--- spec/features/recipes/creating_recipes_spec.rb
+++ spec/features/recipes/creating_recipes_spec.rb
@@ -17,5 +17,42 @@ describe "creating recipes" do
       Recipe.first.name.should == "Candy Corn Dumplings"
       expect(page).to have_content("Candy Corn Dumplings")
     end
+
+    it "should create a recipe with one ingredient" do
+      Ingredient.create(name: 'Spam')
+
+      fill_in 'recipe_name', with: 'Spam Cakes'
+
+      check('Spam Cakes')
+      click_button('Create Recipe')
+
+      Recipe.first.ingredients.where(name: 'Spam Cakes').count.should == 1
+    end
+
+    it "should create a recipe with many ingredients" do
+      Ingredient.create(name: 'Paprika')
+      Ingredient.create(name: 'Clove')
+      Ingredient.create(name: 'Ginger')
+      Ingredient.create(name: 'Cider')
+
+      fill_in 'recipe_name', with: 'Holiday Spice Cider'
+
+      check('Paprika')
+      check('Clove')
+      check('Ginger')
+      check('Cider')
+
+      click_button('Create Recipe')
+
+      Recipe.first.ingredients.count.should == 4
+    end
+
+    it "should create a recipe with 0 ingredients" do
+      fill_in 'recipe_name', with: 'Recipe in Progress'
+
+      click_button('Create Recipe')
+
+      Recipe.first.ingredients.count.should == 0
+    end
   end
 end
