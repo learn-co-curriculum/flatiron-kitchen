1df71b362a6097c85b4ea300400206251592e1c8
Spencer Rogers <spencer1248@gmail.com>
2013-11-06 14:13:20 -0500

    Form + join for recipe ingredients

---
+++ db/migrate/20131106185418_create_recipe_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateRecipeIngredients < ActiveRecord::Migration
+  def change
+    create_table :recipe_ingredients do |t|
+      t.integer :recipe_id
+      t.integer :ingredient_id
+
+      t.timestamps
+    end
+    add_index :recipe_ingredients, :recipe_id
+    add_index :recipe_ingredients, :ingredient_id
+  end
+end
