a40cc5dba10a5eee978d0b5b2b28e1ab6c471664
Spencer Rogers <spencer1248@gmail.com>
2013-11-08 14:35:58 -0500

    Improve solution

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
-  def update_ingredients_by_id(ingredient_ids)
+  def ingredient_ids=(ingredient_ids)
     ingredients = Ingredient.where("id in (?)", ingredient_ids)
     self.ingredients = ingredients
   end
