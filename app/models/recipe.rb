1a12e8a9785ee2994cc5d8aa1dd676f35fc049c2
Spencer Rogers <spencer1248@gmail.com>
2014-02-18 13:41:32 -0500

    Add validations to name fields

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,8 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
+  validates :name, presence: true
+
   def ingredient_ids=(ingredient_ids)
     ingredients = Ingredient.where("id in (?)", ingredient_ids)
     self.ingredients = ingredients
