e93318a13ef45c6c0efac3e0c23b16af26128342
Ian Miller <irmiller22@gmail.com>
2015-03-16 11:19:55 -0400

    updated gemfile, migrations

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :doc do
 end
 
 group :test do
+  gem 'rspec-collection_matchers'
   gem 'rspec-rails'
   gem 'capybara'
 end
--- db/migrate/20131106185418_create_recipe_ingredients.rb
+++ db/migrate/20131106185418_create_recipe_ingredients.rb
@@ -4,7 +4,7 @@ class CreateRecipeIngredients < ActiveRecord::Migration
       t.integer :recipe_id
       t.integer :ingredient_id
 
-      t.timestamps
+      t.timestamps null: false
     end
     add_index :recipe_ingredients, :recipe_id
     add_index :recipe_ingredients, :ingredient_id
