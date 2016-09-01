2f1c73eca81993583468645db2d8c2a7636334de
gabe <gj+code@mail.co.de>
2016-08-31 22:42:43 -0400

    remove unnecessary ingredient_ids= setter method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,9 +3,4 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients, through: :recipe_ingredients
 
   validates :name, presence: true
-
-  def ingredient_ids=(ingredient_ids)
-    ingredients = Ingredient.where("id in (?)", ingredient_ids)
-    self.ingredients = ingredients
-  end
 end
